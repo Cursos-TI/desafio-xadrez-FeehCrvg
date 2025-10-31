164b76ef8add18ff65d407b523a9d5c8d62f133f
FeehCrvg <fevascopaula@gmail.com>
2025-10-31 21:03:41 +0000

    fim

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,50 +1,69 @@
 #include <stdio.h>
 
-int main() {
-
-
-    int casasTorre = 5;
-    printf("=== Movimento da TORRE ===\n");
-    for (int i = 1; i <= casasTorre; i++) {
-        printf("Direita\n");
-    }
-
-
-
-    int casasBispo = 5;
-    int contadorBispo = 1;
-    printf("\n=== Movimento do BISPO ===\n");
-    while (contadorBispo <= casasBispo) {
-        printf("Cima, Direita\n");
-        contadorBispo++;
-    }
- int casasRainha = 8;
-    int contadorRainha = 1;
-    printf("\n=== Movimento da RAINHA ===\n");
-    do {
-        printf("Esquerda\n");
-        contadorRainha++;
-    } while (contadorRainha <= casasRainha);
-    printf("\n");
-
-    printf("=== Movimento do CAVALO ===\n");
-
-    int baixo = 2;
-    int esquerda = 1;
-    
-    for (int x = 1; x <= baixo; x++){
-        printf("Baixo\n");
-
-        if (x== baixo){
-            int y= 1;
-            while (y <= esquerda){
-                printf("Esquerda\n");
-                y++;
-            }
-        }
-    }
-
-    return 0;
+void moverTorre(int casas) {
+    if (casas == 0) return;
+    printf("Direita\n");
+    moverTorre(casas - 1); 
 }
 
 
+void moverBispo(int vertical, int horizontal) {
+    if (vertical == 0 || horizontal == 0) return; 
+    printf("Diagonal superior direita\n");
+    moverBispo(vertical - 1, horizontal - 1); 
+}
+
+
+void moverRainha(int casas) {
+    if (casas == 0) return;
+    printf("Esquerda\n");
+    moverRainha(casas - 1);
+}
+
+void moverCavalo() {
+    int movimentosVerticais = 2;
+    int movimentosHorizontais = 1;
+
+    printf("Movimentos do Cavalo:\n");
+
+    for (int i = 1; i <= movimentosVerticais; i++) {
+        printf("Cima %d\n", i);
+
+        for (int j = 1; j <= movimentosHorizontais; j++) {
+            if (i == 2 && j == 1) {
+                printf("Direita %d\n", j);
+                break; 
+            }
+           
+        }
+    }
+}
+
+
+
+int main() {
+    int casasTorre = 5;
+    int casasBispoV = 3;
+    int casasBispoH = 2;
+    int casasRainha = 8;
+
+    printf("=== Movimento da Torre ===\n");
+    moverTorre(casasTorre);
+
+    printf("\n=== Movimento do Bispo ===\n");
+   
+    for (int i = 0; i < casasBispoV; i++) { 
+        for (int j = 0; j < casasBispoH; j++) { 
+            printf("Diagonal superior direita\n");
+        }
+    }
+
+
+    printf("\n=== Movimento da Rainha ===\n");
+    moverRainha(casasRainha);
+
+    printf("\n=== Movimento do Cavalo ===\n");
+    moverCavalo();
+
+    return 0;
+}
